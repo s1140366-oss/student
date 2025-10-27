9c78a21c0c3f5f7e152e79060bfb93af3141906e
s1140366-oss <s1140366@dmhs.kh.edu.tw>
2025-10-27 16:27:42 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int age = 16;
+    double height = 180.8;
+    std::string name ="歐佩蓁opc";
+    bool is_student = false;
+    std::cout<< "姓名: "<< name<< std::endl;
+    std::cout<< "身高: "<< height<< std::endl;
+    std::cout<< "年齡: "<< age<< std::endl;
+    std::cout<< "是否為學生: "<< is_student<< std::endl;
+
+    return 0;
+}
